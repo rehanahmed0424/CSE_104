c29da8b186fa7f1567ddcf35814ce90f9114abaa
rehanahmed0424 <62881626+rehanahmed0424@users.noreply.github.com>
2023-08-07 10:34:26 +0600

    Add files via upload

---
+++ lab3/mixed pattern.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int N,i,j;
+	scanf("%d",&N);
+
+    for(i=N; i>=1; i--)
+    {
+
+        for(j=N; j>i; j--)
+        {
+            printf("- ");
+
+        }
+                for(j=1; j<=i; j++)
+        {
+               printf("* ");
+
+        }
+        printf("\n");
+    }
+
+	return 0;
+}
