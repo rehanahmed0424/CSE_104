b862eb7fef254dca307ac0d9cfba17008de36820
rehanahmed0424 <62881626+rehanahmed0424@users.noreply.github.com>
2023-08-14 08:39:51 +0600

    Add files via upload

---
+++ lab3/multiplication.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int m,n;
+    scanf("%d %d", &m, &n);
+
+    for(int i=m; i<=n; i++)
+    {
+        printf("Multiplication table for %d: \n", i);
+        printf("\n");
+        for(int j=1; j<=10; j++)
+        {
+            printf("%d X %d = %d\n", j,i, (i*j));
+
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
