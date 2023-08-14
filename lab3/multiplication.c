16c451edde20cd8f51f4ae7655d9d9638d05dae4
rehanahmed0424 <62881626+rehanahmed0424@users.noreply.github.com>
2023-08-14 08:48:03 +0600

    adding comments

--- lab3/multiplication.c
+++ lab3/multiplication.c
@@ -5,11 +5,11 @@ int main()
     int m,n;
     scanf("%d %d", &m, &n);
 
-    for(int i=m; i<=n; i++)
+    for(int i=m; i<=n; i++) //for number of table
     {
         printf("Multiplication table for %d: \n", i);
         printf("\n");
-        for(int j=1; j<=10; j++)
+        for(int j=1; j<=10; j++) //for making table
         {
             printf("%d X %d = %d\n", j,i, (i*j));
 
